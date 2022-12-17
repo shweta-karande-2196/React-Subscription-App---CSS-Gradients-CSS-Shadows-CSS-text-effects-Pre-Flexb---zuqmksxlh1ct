bee72541976265cb23ed6c4d3d8d01bf9b066769
shweta-karande-2196 <117108726+shweta-karande-2196@users.noreply.github.com>
2022-12-17 13:33:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ const App = () => {
   return (
     <div id="main">
       <div class="container">
-                <div class="community">
-                  <h2 class="community-header">Join our community</h2>
+                <div class="community" style={{backgroundColor:"rgb(255, 253, 253)", borderRadius:"11px"}}>
+                  <h2 class="community-header" >Join our community</h2>
                 </div>
                 <div class="card-details">
-                  <div class="card">
+                  <div class="card" style={{backgroundColor:"rgb(15, 67, 66)"}}>
                     <p class="card-header">Monthly Subscription</p>
                   </div>
-                  <div class="why-us">
-                    <h4 class="why-us-header">Why Us</h4>
+                  <div class="why-us" style={{backgroundColor:"rgba(85, 145, 142, 0.698)"}}>
+                    <h4 class="why-us-header" >Why Us</h4>
                   </div>
                 </div>
               </div>
